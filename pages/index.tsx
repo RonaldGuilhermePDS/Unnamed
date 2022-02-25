bf4857fd91aa2322c44e6fd3fd80508512429584
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2022-02-25 08:59:31 -0300

    First Commit

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center w-screen h-screen">
+      <h1 className="text-9xl">Hello, World!</h1>
+    </div>
+  );
+}
